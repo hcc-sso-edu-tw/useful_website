6351a37968f55af761d1367c1dca0def4c89513d
hcc.sso <yenhsiaomu0121@gmail.com>
2025-11-30 20:10:28 +0800

    Delete BSoD-master directory

--- BSoD-master/js/bsod.js
+++
@@ -1,8 +0,0 @@
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-	text: "https://beta.amanotem.com",
-	width: 110,
-	height: 110,
-	colorDark : "#106faa",
-	colorLight : "#ffffff",
-	correctLevel : QRCode.CorrectLevel.H
-});
